1a4ac7ce7be0093be3480a34b1f695d78f0baf05
Daniel Ortega <daniel@gatero.mx>
2015-10-10 10:39:21 -0500

    [boot]: Create test with admin and post models

--- server/boot/create-post.js
+++ server/boot/create-post.js
@@ -1,2 +1,50 @@
+var async = require('async');
+
 module.exports = function(app) {
+  
+  var mysql_ds = app.dataSources.mysql_ds;
+  
+  async.parallel({
+    admins: async.apply( createAdmins )
+  }, function( error, results ){
+    createPosts( results.admins, function( error ){
+      console.log(' > Models created sucessfully !');
+    });
+  });
+
+  function createAdmins( callback ){
+
+    mysql_ds.automigrate('Admin', function( error ){
+      if ( error ) throw error;
+
+      var Admin = app.models.Admin;
+
+      Admin.create([
+        {email: 'daniel@gatero.mx', password: 'bendecida'}
+      ], callback);
+
+    });
+
+  };
+
+  function createPosts( admins, callback ){
+
+    mysql_ds.automigrate('Post', function( error ){
+      if ( error ) return callback( error );
+
+      var Post = app.models.Post;
+      var DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+
+      Post.create([
+        {
+          date: Date.now() - ( DAY_IN_MILLISECONDS * 4 ),
+          lastUpdated: Date.now(),
+          title: 'Titulo de prueba',
+          content: 'Contenido de prueba',
+          adminId: admins[0].id
+        }
+      ], callback);
+
+    });
+  }
 };
